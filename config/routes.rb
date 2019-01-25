01be57ed4de1e36d859bc9da3be246499bf4fc67
jackflaps <kevin@jackflaps.net>
2019-01-25 15:32:05 -0700

    (very simple) new entity form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   match '/login', to: 'sessions#create', via: :post
   match '/logout', to: 'sessions#destroy', via: :delete
 
+  match 'entities/new' => 'entities#new', via: :get
+  match 'entities/new' => 'entities#create', via: :post
+  match 'entities/:id' => 'entities#show', via: :get
+  match 'entities/:id' => 'entities#update', via: :post
+  match 'entities/:id/edit' => 'entities#edit', via: :get
   resources :entities
+
   resources :users
 end
