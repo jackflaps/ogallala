a79b5b9a8535c0afca02a0e69bb981ac42371370
jackflaps <kevin@jackflaps.net>
2019-03-11 12:26:32 -0700

    heroku backend URL

--- og-frontend/src/main.js
+++ og-frontend/src/main.js
@@ -10,7 +10,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'http://og-backend.herokuapp.com'
 
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
